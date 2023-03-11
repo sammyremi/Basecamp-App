6a051bc65379b3a996712e3ef3d2ed88004f952e
sammyremi <adebayorsamuelas@gmail.com>
2023-03-11 19:42:14 +0100

    Route signout isues

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, :controller => {registrations: 'registrations'}
+  resources :projects
+  root 'home#index'
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
